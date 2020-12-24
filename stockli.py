f20f8bfb32be82ef487e24208b70f4549af141da
cberg18 <cberg18@gmail.com>
2020-12-23 23:55:37 -0700

    move imports

--- stockli.py
+++ stockli.py
@@ -1,7 +1,21 @@
 #! /usr/bin/env python3
 import sys
 import os
+import alpaca_trade_api as tradeapi
 
+try:
+    import config
+except ModuleNotFoundError:
+    print('The config file seems to be missing.')
+    print('Use ./stockli.py --config for help setting up the config file.' )
+    quit()
+
+API_KEY = config.API_KEY
+API_SECRET = config.API_SECRET
+BASE_URL = config.BASE_URL
+
+api = tradeapi.REST(API_KEY,API_SECRET,BASE_URL)
+account = api.get_account()
 
 logFile = '/stockli.log'
 oldLogFile = '/var/log/stockli.log.old'
