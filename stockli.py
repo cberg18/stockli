6bcb2dee0372a9e6c938465cb51cdca4ee55c3c3
Cory Berg <cberg18@gmail.com>
2020-12-28 23:09:26 -0700

    key handling, endpoint  checking

--- stockli.py
+++ stockli.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python3
 # TODO: trend detector
-# TODO: get calc current rating 
+# TODO: calc current rating 
 # TODO: check if you have sufficient funds to process a buy order
 
 import sys
@@ -15,9 +15,12 @@ except ModuleNotFoundError:
     print('Use ./stockli.py --config for help setting up the config file.')
     quit()
 
-API_KEY = config.API_KEY
-API_SECRET = config.API_SECRET
-BASE_URL = config.BASE_URL
+trading_type = 'paper' 
+
+API_KEY = config.PAPER_API_KEY
+API_SECRET = config.PAPER_API_SECRET
+BASE_URL = config.PAPER_BASE_URL
+
 
 api = tradeapi.REST(API_KEY, API_SECRET, BASE_URL)
 account = api.get_account()
@@ -43,14 +46,15 @@ def helpString():
     Usage: ./stockli.py [OPTION]...
 
     Options:
-     -h, --help                     prints this page and exits.
-     -u, --update                   checks for updates.
-     -s [SYMBOL]                    grabs relevant information for [SYMBOL]
-     --market-status                returns if market is open or closed
-     --list-current-positions       list all open positions
-     --buy [SYMBOL] [QUANTITY]      buy specified quantitiy of a stock
-     --sell [SYMBOL] [QUANTITY]     sell specified quantity of a stock
-     --track [SYMBOL] [PERIOD]      track a symbol at sepcified interval until interupted, default 2m (valid periods: 1m,2m,5m,15m,30m,60m,90m,1h,1d,5d,1wk,1mo,3mo)
+    -h, --help                                      prints this page and exits.
+    -u, --update                                    checks for updates.
+    -s [SYMBOL]                                     grabs relevant information for [SYMBOL]
+    --market-status                                 returns if market is open or closed
+    --list-current-positions                        list all open positions
+    --buy [SYMBOL] [QUANTITY]                       buy specified quantitiy of a stock
+    --sell [SYMBOL] [QUANTITY]                      sell specified quantity of a stock
+    --track [SYMBOL] [PERIOD]                       track a symbol at sepcified interval until interupted, default 2m (valid periods: 1m,2m,5m,15m,30m,60m,90m,1h,1d,5d,1wk,1mo,3mo)
+    --trade_type                                    gets currently used trading api, paper or live. CAUTION: AT THIS TIME, STOCKLI HAS NOT BEEN TESTED IN A LIVE ENVIRONMENT. USE AT YOUR OWN RISK.
 
     stockli is pre-pre-pre-pre-pre alpha and comes with NO WARRANTY.
     If you're seeing this, you probably shouldn't be. Shame on you.
@@ -176,10 +180,19 @@ if __name__ == '__main__':
             elif order.status == 'accepted':
                 print(
                     'Your order was accepted by Alpaca, but has not ben routed to be executed.')
+
     elif (sys.argv[1] == '--track'):
         if len(sys.argv) == 4: period = str(sys.argv[3])
 
         tracker(sys.argv[2].upper(), period)
+
+    elif (sys.argv[1] == '--trade-type'):
+        if len(sys.argv) < 3: 
+            print('Not enough arguments, use -h or --help for more information. ')
+            quit()
+        if sys.argv[2].lower() == 'current': print('Stockli is set to use the ' + trading_type + ' endpoint.')
+#        if sys.argv[2].lower() == 'set' and len(sys.argv) < 4 : print('Please specify either live or paper api. ')
+#        if sys.argv[2].lower() == 'set' and ((sys.argv[3].lower() == 'live') or (sys.argv[3].lower() == 'paper')) : config.trading_type = trading_type = sys.argv[3].lower()
 
     else:
         print('Specified option not recognized. Do main.py -h or --help for help.')
