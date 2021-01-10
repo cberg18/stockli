d8ed997a20d4daffa3a61f743d87e94b82982090
Cory Berg <cberg18@gmail.com>
2021-01-09 23:55:38 -0700

    unused

--- utils/sma.py
+++
@@ -1,8 +0,0 @@
-def sma(dataframe, window):
-    '''
-    Calculate sma for a given period.
-    '''
-    name = 'SMA ' + str(window)
-
-    dataframe[name] = dataframe['Close'].rolling(window).mean().round(2)
-    return dataframe
