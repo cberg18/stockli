8e418ccfd5dd628c43d5f5cd0e2e7556cc5f9f01
Cory Berg <cberg18@gmail.com>
2021-02-09 20:51:29 -0700

    fix order of operations

--- update.sh
+++ update.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
+echo "Checking for updates..."
+git fetch
+
 UPSTREAM=${1:-'@{u}'}
 LOCAL=$(git rev-parse @)
 REMOTE=$(git rev-parse "$UPSTREAM")
 
-echo "Checking for updates..."
-git fetch
-
-if [ $LOCAL != $REMOTE ]
-then
+if [ $LOCAL != $REMOTE ]; then
     echo "Update available, pulling..."
     git pull
-else [ $LOCAL = $REMOTE ]
+else
+    [ $LOCAL = $REMOTE ]
     echo "No updates available."
 fi
